18b61db29edcfa77ceb5731b7788303139801eec
melaniefann <melanie.x.fann@vanderbilt.edu>
2022-02-22 10:08:13 -0600

    lolz

--- myplayer.py
+++ myplayer.py
@@ -2,6 +2,23 @@ from player import Player
 import numpy as np
 
 
+def get_valid_moves(board) -> []:
+    validMoves = []
+    for i in range(0, board.shape[0]):
+        if board[0][i] == 0:
+            np.append(validMoves, i)
+    return validMoves
+
+
+def make_new_board(board, col, value) -> np.ndarray:
+    newBoard = board.copy()
+    for i in range(board.shape[0], 0, -1):
+        if newBoard[i][col] == 0:
+            newBoard[i][col] = value
+            break
+    return newBoard
+
+
 class MyPlayer(Player):
     def setup(self):
         """
@@ -38,55 +55,53 @@ class MyPlayer(Player):
             to the lowest empty row in the column (like a pile).
         """
 
-        raise NotImplementedError()
+        return alpha_beta(self, board, 10, math.inf, -math.inf, True, None)
 
-    def getValidMoves(self, board) -> []:
-        validMoves = []
-        for i in range(0, board.shape[0]):
-            if board[0][i] == 0:
-                np.append(validMoves, i)
-        return validMoves
-
-    def isTerminalNode(self, board) -> bool:
+    def is_winning_board(self, board, column) -> bool:
+        # check the most recent piece that was added to the board to see if it wins
         pass
 
     def heuristic(self, board) -> int:
+        # needs to check all of them *****
+        # horizontal
+
+        # vertical
+
+        # right diagonals
+
+        # left diagonals
         pass
 
-    def makeNewBoard(self, board, col, value) -> np.ndarray:
-        newBoard = board.copy()
-        for i in range(board.shape[0], 0, -1):
-            if newBoard[i][col] == 0:
-                newBoard[i][col] = value
-                break
-        return newBoard
-
-
-
-    def alphabeta(self, board, depth, alpha, beta, maximizingPlayer) -> (int, int):
-        validMoves = getValidMoves()
-        isTerminal = isTerminalNode(board)
+    def alpha_beta(self, board, depth, alpha, beta, maximizing, lastColumn) -> (int, int):
+        won = is_winning_board(board, lastColumn)
         columnToPlay = None
-        if depth == 0 or isTerminal:
+
+        if won:
+            if maximizing: # need to check this
+                return columnToPlay, math.inf
+            else:
+                return columnToPlay, -math.inf
+
+        validMoves = get_valid_moves(board)
+        if depth == 0 or len(validMoves) == 0:
             return columnToPlay, heuristic(board)
-        if maximizingPlayer:
-            bestHeuristic = -10000000
+        if maximizing:
+            bestHeuristic = -math.inf
             for column in validMoves:
-                newBoard = makeNewBoard(self, board, column, +1)
-                curHeuristic = alphabeta(self, newBoard, alpha, beta, False)
+                newBoard = make_new_board(board, column, +1)
+                curHeuristic = alpha_beta(self, newBoard, alpha, beta, False, column)[1]
                 if curHeuristic > bestHeuristic:
                     bestHeuristic = curHeuristic
                     columnToPlay = column
-
                 if value >= beta:
                     break
                 alpha = max(alpha, value)
                 return columnToPlay, value
         else:
-            bestHeuristic = 10000000
+            bestHeuristic = math.inf
             for column in validMoves:
-                newBoard = makeNewBoard(self, board, column, -1)
-                curHeuristic = alphabeta(self, newBoard, alpha, beta, True)
+                newBoard = make_new_board(board, column, -1)
+                curHeuristic = alpha_beta(self, newBoard, alpha, beta, True, column)[1]
                 if curHeuristic < bestHeuristic:
                     bestHeuristic = curHeuristic
                     columnToPlay = column
